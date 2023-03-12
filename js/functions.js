2602de0afdc66321d0e1d24d48afdeb0d955724f
Greg <grtop@yandex.ru>
2023-03-12 19:48:55 +0300

    with data

---
+++ js/functions.js
@@ -0,0 +1,26 @@
+import {getPictures} from './data.js';
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const container = document.querySelector('.pictures');
+
+const createPhoto = ({url, description, comments, likes}) => {
+  const picElem = pictureTemplate.cloneNode (true);
+  picElem.querySelector('.picture_img').src = url;
+  picElem.querySelector('.picture_img').alt = description;
+  picElem.querySelector('.picture_comments').textContent = comments.length;
+  picElem.querySelector('.picture_likes').textContent = likes;
+
+  return picElem;
+};
+
+const renderPhotos = (pictures) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    const picElem = createPhoto (picture);
+    fragment.append(picElem);
+  });
+  container.append(fragment);
+};
+
+export {renderPhotos};
+
+
--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
-import './data.js';
-import './utils.js';
-import './functions.js';
-import './global-const.js';
-import './forms.js';
-import './generator.js';
-import './button-actions.js';
-import './effects.js';
+import {getPictures} from './data.js';
+import {renderPhotos} from './functions.js';
+
+renderPhotos(getPictures());
