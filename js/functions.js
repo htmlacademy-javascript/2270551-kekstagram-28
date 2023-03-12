18961d70599d028dd111a7112f4288962d0b6c69
Greg <grtop@yandex.ru>
2023-03-12 22:04:41 +0300

    решение 2 с двумя функциями

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,24 @@
+
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const fragment = document.createDocumentFragment();
 const container = document.querySelector('.pictures');
 
-const renderPhotos = (array) => {
-  array.forEach (({url, description, comments, likes}) => {
-    const picElem = pictureTemplate.cloneNode (true);
-    picElem.querySelector('.picture__img').src = url;
-    picElem.querySelector('.picture__img').alt = description;
-    picElem.querySelector('.picture__comments').textContent = comments.length;
-    picElem.querySelector('.picture__likes').textContent = likes;
-    fragment.appendChild(picElem);
+const createPhoto = ({url, description, comments, likes}) => {
+  const picElem = pictureTemplate.cloneNode (true);
+  picElem.querySelector('.picture__img').src = url;
+  picElem.querySelector('.picture__img').alt = description;
+  picElem.querySelector('.picture__comments').textContent = comments.length;
+  picElem.querySelector('.picture__likes').textContent = likes;
+
+  return picElem;
+};
+
+const renderPhotos = (pictures) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    const picElem = createPhoto (picture);
+    fragment.append(picElem);
   });
-  return container.appendChild(fragment);
+  container.append(fragment);
 };
 
 export {renderPhotos};
