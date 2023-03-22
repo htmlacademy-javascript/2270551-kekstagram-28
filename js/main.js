e7665422946be5cfb248f1592e4e236b5a87948d
Greg <grtop@yandex.ru>
2023-03-22 10:43:49 +0300

    проверка версии

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-import {pictureData} from './data.js';
-import {renderPhotos} from './pictures.js';
+import './mock.js';
+import './utils.js';
+import './data.js';
+import './image-elements.js';
+import './big-picture.js';
 
 renderPhotos(pictureData);
