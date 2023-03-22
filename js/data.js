872e5a179e5d6bfd924f139141c704e9d6a04197
Greg <grtop@yandex.ru>
2023-03-22 14:51:57 +0300

    vozvr2

--- js/data.js
+++ js/data.js
@@ -1,89 +1,64 @@
-import {getRandomInteger} from './utils.js';
-
-const NAMES = [
-  'Павел Позняк',
-  'Екатерина Некрылова',
-  'Александр Страховенко',
-  'Балтика Пивандров',
-  'Сигарет Курилов',
-  'Евгений Мухоморов'
-];
-
-const DESCRIPTIONS = [
-  'Чиллю на отдыхе',
-  'Кот Кекс спит на диване',
-  'Это мы прогуливаем пары',
-  'С друзьями решили сдать бутылки',
-  'Курим у падоса',
-  'Какая шикарная погодка...',
-  'Отпуск в Турции',
-  'Зоопарк',
-  'Голубое небо',
-  'Аквариум с рыбками',
-  'Вкусный ужин в ресторане',
-  'Пошли с друзьями в бар',
-  'Играем в волейбол',
-  'Соревнование по баскетболу',
-  'Пошли качаться в зал',
-  'Моя тачка',
-  'Синяя птичка',
-  'Мой любимый сидр',
-  'Бургер в Макухе',
-  'Дождь в Иркутске',
-  'Вышел прогуляться',
-  'Острые крылышки',
-  'Любимый вариант завтрака',
-  'На пробежке',
-  'Пошел выносить мусор'
-];
-
-const MESSAGES = [
-  'Всё отлично!',
+const PHOTO_COUNT = 25;
+const AVATAR_COUNT = 6;
+const LIKE_COUNT_MIN = 15;
+const LIKE_COUNT_MAX = 200;
+const COMENT_COUNT = 20;
+const AVTOR_NAMES = ['Ivan', 'Boris', 'Svetlana', 'John'];
+const MES_AVTORS = ['Всё отлично!',
   'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
+  'В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+const DESCRIPT_PHOTO = ['прекрасное настроение', 'как-то так!', 'долго старался чтоб поймать момент','лучший кадр!'];
 
-const POSTS_COUNT = 25;
-
-let postId = 1;
-let commentId = 1;
-
-const createMessage = (messages) => {
-  const message = Array.from({length: getRandomInteger(1, 2)}, () => messages[getRandomInteger(0, messages.length - 1)]);
-
-  return [...new Set(message)].join(' ');
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
 
-const createCommentData = () => {
-  const comment = {
-    id: commentId,
-    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-    message: createMessage(MESSAGES),
-    name: NAMES[getRandomInteger(0, NAMES.length - 1)]
+const getRandomArrayElement = (array) =>
+  array[getRandomInteger(0, array.length - 1)];
+
+const createIdGenerator = () => {
+  let lastGeneratedId = 0;
+  return () => {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
   };
-
-  commentId++;
-
-  return comment;
 };
 
-const createPostData = () => {
-  const post = {
-    id: postId,
-    url: `photos/${postId}.jpg`,
-    description: DESCRIPTIONS[getRandomInteger(0, DESCRIPTIONS.length - 1)],
-    likes: getRandomInteger(15, 200),
-    comments: Array.from({length: getRandomInteger(3, 11)}, createCommentData)
-  };
+const generatedCommentId = createIdGenerator();
 
-  postId++;
+const createMessage = () => Array.from({length: getRandomInteger(1, 2)}, () =>
+  getRandomArrayElement(MES_AVTORS)).join('');
 
-  return post;
-};
+const createComment = () => ({
+  id: generatedCommentId(),
+  avatar: `img/avatar-${getRandomInteger(1, AVATAR_COUNT)}.svg`,
+  message: createMessage(),
+  name: getRandomArrayElement(AVTOR_NAMES),
+});
 
-const createPostsDataset = () => Array.from({length: POSTS_COUNT}, createPostData);
+const createPicture = (index) => ({
+  id: index,
+  url: `photos/${index}.jpg`,
+  description: getRandomArrayElement(DESCRIPT_PHOTO),
+  likes: getRandomInteger(LIKE_COUNT_MIN, LIKE_COUNT_MAX),
+  comments: Array.from(
+    {length: getRandomInteger(0, COMENT_COUNT)},
+    createComment
+  ),
+});
 
-export {createPostsDataset};
+const getPictures = () =>
+  Array.from({length: PHOTO_COUNT}, (_, pictureIndex) =>
+    createPicture(pictureIndex + 1)
+  );
+const pictureData = getPictures();
+
+export {pictureData};
+
--- js/utils.js
+++ js/utils.js
@@ -1,2 +1,3 @@
 const isEscapeKey = (evt) => evt.key === 'Escape';
 export {isEscapeKey};
+
