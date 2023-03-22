21c1f52e80a348ee7002facfbfa0d5b2071e08ee
Greg <grtop@yandex.ru>
2023-03-22 17:13:02 +0300

    fix3

--- js/pictures.js
+++ js/pictures.js
@@ -2,13 +2,14 @@
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const container = document.querySelector('.pictures');
 
-const createPhoto = ({url, description, comments, likes}) => {
+
+const createPhoto = ({url, description, comments, likes, id}) => {
   const picElem = pictureTemplate.cloneNode (true);
   picElem.querySelector('.picture__img').src = url;
   picElem.querySelector('.picture__img').alt = description;
   picElem.querySelector('.picture__comments').textContent = comments.length;
   picElem.querySelector('.picture__likes').textContent = likes;
-
+  picElem.dataset.thumbnailId = id;
 
   /* picElem.addEventListener('click', (evt) => {
     evt.preventDefault();
