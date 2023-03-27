ebb4c8b503196ed5f16a29026aec4d3c7e631a5a
Greg <grtop@yandex.ru>
2023-03-27 16:50:09 +0300

    module8-task1

--- js/gallery.js
+++ js/gallery.js
@@ -4,9 +4,18 @@ import {renderBigPictureData, bigPicture} from './big-picture.js';
 import {isEscKey, isEnterKey} from './utils.js';
 
 
+/* количество коментов на страничке */
+const COMMENTS_COUNT = 5;
+
 const bigPictureCloseElement = bigPicture.querySelector('.big-picture__cancel');
 const commentCount = document.querySelector('.social__comment-count');
 const commentsLoader = document.querySelector('.comments-loader');
+const commentList = document.querySelector('.social__comments');
+
+
+let commentsShown = 0;
+let comments = [];
+
 
 /* 5 убираем большую картинку по кнопке esc */
 const onDocumentKeydown = (evt) => {
@@ -25,6 +34,42 @@ const createComment = ({avatar, message, name}) => {
   comment.querySelector('.social__picture').alt = name;
   comment.querySelector('.social__text').textContent = message;
   return comment;
+};
+
+const renderComments = () => {
+  /* открытие и закрытие коментариев более 5 в большом окне */
+  commentsShown += COMMENTS_COUNT;
+  if (commentsShown >= comments.length) {
+    commentsLoader.classList.add('hidden');
+    commentsShown = comments.length;
+  } else {
+    commentsLoader.classList.remove('hidden');
+  }
+
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < commentsShown; i++) {
+    const commentElement = createComment(comments[i]);
+    fragment.append(commentElement);
+  }
+
+  /* функция проверки склонения слова после числа */
+  function commentsDeclination(n, textForms) {
+    n = Math.abs(n) % 100;
+    const n1 = n % 10;
+    if (n1 > 1) {
+      return textForms[1];
+    }
+    if (n1 === 1 && n1 !== 11) {
+      return textForms[0];
+    }
+    return textForms[1];
+  }
+  /* заменяем содержимое класса .social__comments на пустую строку */
+  commentList.innerHTML = '';
+  /* вставляем в содержимое класса .social__comments данные из fragment */
+  commentList.append(fragment);
+  /* формируем сообщение о количестве комментариев */
+  commentCount.innerHTML = `${commentsShown} из <span class="comments-count">${comments.length}</span> ${commentsDeclination(comments.length, ['коментария', 'коментариев'])}`;
 };
 
 /* открытие большого окна по нажатию на миниатюру */
