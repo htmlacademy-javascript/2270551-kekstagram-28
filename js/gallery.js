423f906c3552ff64493957ba8fe104cfea5073a1
Greg <grtop@yandex.ru>
2023-04-08 12:50:58 +0300

    убрал коменты

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,9 @@
-/* 1 модуль отрисовки окна с полноразмерным изображением */
+
 import {createPictures, pictureContainer} from './pictures.js';
 import {renderBigPictureData, bigPicture} from './big-picture.js';
 import {isEscKey, isEnterKey} from './utils.js';
 
-/* количество коментов на страничке */
+
 const COMMENTS_COUNT = 5;
 
 const bigPictureCloseElement = bigPicture.querySelector('.big-picture__cancel');
@@ -13,10 +13,7 @@ const commentList = document.querySelector('.social__comments');
 
 let commentsShown = 0;
 let comments = [];
-//let pictures = [];
 
-
-/* 5 убираем большую картинку по кнопке esc */
 const onDocumentKeydown = (evt) => {
   if (isEscKey(evt)) {
     evt.preventDefault();
@@ -24,7 +21,6 @@ const onDocumentKeydown = (evt) => {
   }
 };
 
-/* 2 разметка комментариев под большой фотографией */
 const createComment = ({avatar, message, name}) => {
   const comment = document.createElement('li');
   comment.innerHTML = '<img class="social__picture" src="" alt="" width="35" height="35"><p class="social__text"></p>';
@@ -36,7 +32,7 @@ const createComment = ({avatar, message, name}) => {
 };
 
 const renderComments = () => {
-  /* открытие и закрытие коментариев более 5 в большом окне */
+
   commentsShown += COMMENTS_COUNT;
   if (commentsShown >= comments.length) {
     commentsLoader.classList.add('hidden');
@@ -51,18 +47,7 @@ const renderComments = () => {
     fragment.append(commentElement);
   }
 
-  /* функция проверки склонения слова после числа */
-  /*function commentsDeclination(n, textForms) {
-    n = Math.abs(n) % 100;
-    const n1 = n % 10;
-    if (n1 > 1) {
-      return textForms[1];
-    }
-    if (n1 === 1 && n1 !== 11) {
-      return textForms[0];
-    }
-    return textForms[1];
-  }*/
+
   const numDecline = (num, nominative, genitiveSingular, genitivePlural) => {
     if (num > 10 && (Math.round((num % 100) / 10)) === 1) {
       return genitivePlural;
@@ -74,21 +59,17 @@ const renderComments = () => {
     return genitivePlural;
   };
 
-  /* заменяем содержимое класса .social__comments на пустую строку */
   commentList.innerHTML = '';
-  /* вставляем в содержимое класса .social__comments данные из fragment */
+
   commentList.append(fragment);
-  /* формируем сообщение о количестве комментариев */
+
   commentCount.innerHTML = `${commentsShown} из <span class="comments-count">${comments.length}</span> ${numDecline(comments.length,'коментарий','коментария','коментариев')}`;
 };
 
-/* открытие большого окна по нажатию на миниатюру */
+
 const openModal = (picture) => {
-  /* убираем hidden показываем большую картинку */
   bigPicture.classList.remove('hidden');
-  /* 4 добавляем тегу body класс modal-open чтобы убрать прокрутку */
   document.body.classList.add('modal-open');
-  /* соответствие миниатюры и большой фото */
   document.addEventListener('keydown', onDocumentKeydown);
   renderBigPictureData(picture);
   comments = picture.comments;
@@ -98,13 +79,11 @@ const openModal = (picture) => {
   }
 };
 
-/* функция закрытия большого окна */
 function closeModal () {
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
 }
-/* отслеживание события "клик" для закрытия окна */
 bigPictureCloseElement.addEventListener('click', () => {
   closeModal();
 });
